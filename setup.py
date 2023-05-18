40fdf40b495956d082bfdb5a7cfd409e7f6eec98
Cory Zue <cory@coryzue.com>
2023-05-18 15:13:46 +0200

    bump again

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='markdown-emdash',
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['mdx_emdash'],
     include_package_data=True,
     license='MIT License',
